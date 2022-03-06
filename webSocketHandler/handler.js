9d9cb3df2efa0254fa9b5d6a72c1fa9fc267b795
Atsukoro1 <dornicakkuba@gmail.com>
2022-03-06 18:58:54 +0100

    Deciphering data

--- webSocketHandler/handler.js
+++ webSocketHandler/handler.js
@@ -4,14 +4,14 @@ const crypto = require("crypto");
 const { parseReceived } = require("./parser");
 
 // We'll cache all connected clients here to prevent DDOS
-const connectedClients = new Map();
+const CONNECTEDCLIENTS = new Map();
 
 const webSocketHandler = function(req, socket) {
     // Get client's ip address in order to identify it
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
     // Check if client is already connected
-    // if(connectedClients.get(ip)) return socket.end("HTTP/1.1 429 Too Many Requests\r\n\r\n");
+    if(CONNECTEDCLIENTS.get(ip)) return socket.end("HTTP/1.1 429 Too Many Requests\r\n\r\n");
 
     // Check for correct headers
     // Client must specify Upgrade and Subprotocol header
@@ -25,22 +25,26 @@ const webSocketHandler = function(req, socket) {
 
     // Create Accept key hash in order to accpet the handshake
     function createAcceptKey() {
-        const shasum = crypto.createHash('sha1');
-        shasum.update(req.headers["sec-websocket-key"] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
-        return shasum.digest().toString("base64");
+        // Create SHA-1 HASH
+        const SHASUM = crypto.createHash('sha1');
+
+        // Assert the value into it and return it base64 encoded
+        SHASUM.update(req.headers["sec-websocket-key"] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+        return SHASUM.digest().toString("base64");
     }
 
     if(!req.headers['sec-websocket-protocol'] || !req.headers['sec-websocket-protocol'].split(",").includes("json")) return socket.end("HTTP/1.1 400 Bad Request\r\n\r\n")
     // Construct response and return it back to client
-    const response = `HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ${createAcceptKey()}\r\nSec-Websocket-Protocol: json\r\n\r\n`
-    socket.write(response);
+    const RESPONSE = `HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ${createAcceptKey()}\r\nSec-Websocket-Protocol: json\r\n\r\n`
+    socket.write(RESPONSE);
 
     // Handshake was successful, cache client
-    connectedClients.set(ip, true);
+    CONNECTEDCLIENTS.set(ip, true);
 
     // Receive all data from client
     socket.on("data", data => {
-        parseReceived(data);
+        const CONTENT = parseReceived(data);
+        console.log(CONTENT);
     });
 }
 
--- webSocketHandler/parser.js
+++ webSocketHandler/parser.js
@@ -1,9 +1,9 @@
 function parseReceived(buff) {
     // Content is final frame in binary, represented Boolean
-    const isFinalFrame = Boolean(buff[0].toString(2)[0]);
+    const ISFINALFRAME = Boolean(buff[0].toString(2)[0]);
 
     // Reserved flags represented in binary
-    const reservedFlags = buff[0].toString(2).substring(1, 4);
+    const RESERVEDFLAGS = buff[0].toString(2).substring(1, 4);
 
     // OP Code represented in decimal
     // 0 -> Continuation frame / 0x0
@@ -12,18 +12,44 @@ function parseReceived(buff) {
     // 8 -> Connection close frame / 0x8
     // 9 -> Ping frame (Heartbeat received) / 0x9
     // 10 -> Pong frame (Heartbeat response) / 0xA
-    const opCode = parseInt(buff[0].toString(2).substring(5, 8), 2);
+    const OPCODE = parseInt(buff[0].toString(2).substring(5, 8), 2);
 
     // Payload is masked represented in binary represented as Boolean
-    const isMasked = Boolean(buff[1].toString(2)[0]);
+    const ISMASKED = Boolean(buff[1].toString(2)[0]);
 
     // Received string's length represented in decimal
-    const contentLength = buff[1] - 0x80;
+    let contentLength = buff[1] - 0x80;
 
     // Check if content's length is bigger than 125,
     // content's opcode is something else than 1,
     // if so, return nothing.
-    if(contentLength > 125 || opCode !== 1) return;
+    if(contentLength > 125 || OPCODE !== 1 || ISMASKED === false) return;
+
+    // We need to skip first two bytes containing the data about the payload
+    let offset = 2;
+
+    // Check if payload is bigger than 125 and equals 126
+    // If so, read content length from there
+    if(contentLength > 125 && contentLength === 126) {
+        contentLength = buff.readUInt16BE(offset);
+        offset += 2;
+    };
+
+    // Get the Masking key because content is masked
+    let maskingKey = buff.readUInt32BE(offset);
+    offset += 4;
+
+    // Allocate data for payload and unmask it
+    const data = Buffer.alloc(contentLength);
+    for (let i = 0, j = 0; i < contentLength; i++, j = i % 4) {
+        const SHIFT = j === 3 ? 0 : (3 - j) << 3;
+        const MASK = (SHIFT == 0 ? maskingKey : (maskingKey >>> SHIFT)) & 0xFF;
+        const SOURCE = buff.readUInt8(offset++);
+        data.writeUInt8(MASK ^ SOURCE, i);
+    };
+
+    // Return data utf-8 encoded
+    return data.toString('utf-8');
 }
 
 module.exports = { parseReceived };
